0f7cdd2dff4f2b3a4963bd3bb2373f723605d2ac
rafalbg <rafalbg@users.noreply.github.com>
2020-06-16 21:30:00 +0200

    gut

--- src/Main.java
+++ src/Main.java
@@ -10,71 +10,60 @@ public class Main {
         System.out.println("Hello World!");
 
 
+        String[] try1 = new String[100];
+        try1[0] = "192.168.1.";
+
+        ip_check[] array_of_ips = new ip_check[100];
 
 
+        for (int i = 1; i < 100; i++) {
+            array_of_ips[i] = new ip_check();
+            try1[i] = try1[0] + Integer.toString(i);
+            array_of_ips[i].setIpv4(try1[i]);
+        }
 
-String [] try1 =  new   String [100] ;
-try1[0]="192.168.1.";
-
-        ip_check [] array_of_ips  = new ip_check[100];
-
-
-for (   int i=1; i<100; i++) {
-    array_of_ips[i]= new ip_check();
-    try1[i] = try1[0] + Integer.toString(i);
-array_of_ips[i].setIpv4(try1[i]);
-}
-
-try1[0]="192.168.1.1";
-array_of_ips[0]= new ip_check();
+        try1[0] = "192.168.1.1";
+        array_of_ips[0] = new ip_check();
 
         array_of_ips[0].setIpv4(try1[0]);
 
 
-
-        for (   int i=1; i<100; i++) {
+        for (int i = 1; i < 100; i++) {
 
             array_of_ips[i].start();
 
         }
 
 
+    }
+
+    public static String get_ipv4() {
+
+
+        Scanner key_get = new Scanner(System.in);
+
+        String ip_gotten = key_get.nextLine();
+
+        return ip_gotten;
 
 
     }
-public static String get_ipv4(){
-
-
-    Scanner key_get = new Scanner(System.in);
-
-    String ip_gotten=key_get.nextLine();
-
-    return ip_gotten;
-
 
 
 }
 
 
-
-}
-
-
-
-
-
-
-class  ip_check extends Thread{
+class ip_check extends Thread {
 
 
     public String getIpv4() {
         return ipv4;
     }
 
- // public ip_check(String ipv4){
-   //  /   this.ipv4 =ipv4;
+    // public ip_check(String ipv4){
+    //  /   this.ipv4 =ipv4;
 
- // }
+    // }
 
     public void setIpv4(String ipv4) {
         this.ipv4 = ipv4;
@@ -82,34 +71,28 @@ class  ip_check extends Thread{
 
     String ipv4;
 
-public ip_check(){
+    public ip_check() {
 
-    ipv4="";
+        ipv4 = "";
 
-}
-        public void run(){
+    }
 
-            try {
-                InetAddress ipv4_innet_check = InetAddress.getByName(ipv4);
+    public void run() {
 
-          if( ipv4_innet_check.isReachable(1000))
-              System.out.println("address "+ipv4+" is alive ");
-          else                System.out.println("address "+ipv4+" is ded ");
+        try {
+            InetAddress ipv4_innet_check = InetAddress.getByName(ipv4);
 
-
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            if (ipv4_innet_check.isReachable(1000))
+                System.out.println("address " + ipv4 + " is alive ");
+            else System.out.println("address " + ipv4 + " is ded ");
 
 
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
-
-
+    }
 
 
 }
