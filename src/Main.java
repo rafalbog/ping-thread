47d56f130b86cee38aaf848b34298d1897bd4a2f
rafalbg <rafalbg@users.noreply.github.com>
2020-06-17 20:52:07 +0200

    gut

--- src/Main.java
+++ src/Main.java
@@ -15,38 +15,25 @@ public class Main {
 String some_string= "192.168.1.";
         ip_check[] array_of_ips = new ip_check[100];
 
-
         for (int i = 0; i < 100; i++) {
             array_of_ips[i] = new ip_check(some_string+Integer.toString(i));
         //    try1[i] = try1[0] + Integer.toString(i);
            // array_of_ips[i].setIpv4(try1[i]);
+            array_of_ips[i].start();
         }
 
         try1[0] = "192.168.1.1";
         array_of_ips[0] = new ip_check();
-
       //  array_of_ips[0].setIpv4(try1[0]);
-
-
-        for (int i = 1; i < 100; i++) {
-
-            array_of_ips[i].start();
-
-        }
-
-
+        //for (int i = 1; i < 100; i++) {
+       //     array_of_ips[i].start();
+       // }
     }
 
     public static String get_ipv4() {
-
-
         Scanner key_get = new Scanner(System.in);
-
         String ip_gotten = key_get.nextLine();
-
         return ip_gotten;
-
-
     }
 
 
@@ -55,45 +42,32 @@ String some_string= "192.168.1.";
 
 class ip_check extends Thread {
 
-
     public String getIpv4() {
         return ipv4;
     }
 
      public ip_check(String ipv4){
       this.ipv4 =ipv4;
-
     }
-
   //  public void setIpv4(String ipv4) {
    //     this.ipv4 = ipv4;
    // }
-
     String ipv4;
-
-    public ip_check() {
-
-        ipv4 = "";
-
-    }
+    public ip_check() { ipv4 = ""; }
 
     public void run() {
-
-        try {
-            InetAddress ipv4_innet_check = InetAddress.getByName(ipv4);
+        try { InetAddress ipv4_innet_check = InetAddress.getByName(ipv4);
 
             if (ipv4_innet_check.isReachable(1000))
                 System.out.println("address " + ipv4 + " is alive ");
-            else System.out.println("address " + ipv4 + " is ded ");
 
+            else System.out.println("address " + ipv4 + " is ded ");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
-
 
 }
 
